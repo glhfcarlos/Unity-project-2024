582df5e1337d4687711782aa8bfb66600eb9c441
CapFoxy1001 <adencampbell70@gmail.com>
2024-08-02 22:47:02 -1000

    NPC interation

--- Gerascophobia/Assets/Script/Player Movement.cs
+++ Gerascophobia/Assets/Script/Player Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private NPC_Controller npc;
    
     public float moveSpeed = 5;
 
@@ -23,29 +24,52 @@ public class PlayerMovement : MonoBehaviour
 
 
 
-    void Update()
+    private void Update()
     {
-
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        
-        if (movement.x != 0  || movement.y != 0){
-        
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-
-        animator.SetBool("IsWalking", true);
-        } else{
-            animator.SetBool("IsWalking", false);
+        if (!inDialogue())
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
         }
-       
+        if (movement.x != 0  || movement.y != 0){
+                    
+                    animator.SetFloat("Horizontal", movement.x);
+                    animator.SetFloat("Vertical", movement.y);
 
-
+                    animator.SetBool("IsWalking", true);
+                    } else{
+                        animator.SetBool("IsWalking", false);
+                    }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    
+        private bool inDialogue()
+    {
+        if (npc != null)
+            return npc.DialogueActive();
+        else
+            return false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "NPC")
+        {
+            npc = collision.gameObject.GetComponent<NPC_Controller>();
+
+            if (Input.GetKey(KeyCode.E))
+                npc.ActivateDialogue();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        npc = null;
     }
 
 }
