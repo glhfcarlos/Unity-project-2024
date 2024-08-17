3c5790131d6a90fdcd862b2250154fcb8340200e
earthtoviny <vinyzacarbajalcabrera@gmail.com>
2024-08-16 21:45:38 -0700

    hina

--- Gerascophobia/Assets/Script/Character Battle.cs
+++ Gerascophobia/Assets/Script/Character Battle.cs
@@ -8,7 +8,7 @@ public class CharacterBattle : MonoBehaviour {
    private void Awake () {
     playermovement = GetComponent<PlayerMovement>();
    }
-    private void Start() {
-        PlayerMovement.movement.x(new Vector3(1,0));
+    private void Update() {
+        playermovement.movement = new Vector2(0.25f,0);
     }
 } 
