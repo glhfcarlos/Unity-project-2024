8f0f23acfdb5d8b823720c6b5b4826672b7ac53a
CapFoxy1001 <adencampbell70@gmail.com>
2024-08-20 18:47:35 -1000

    Dialogue input

--- Gerascophobia/Assets/Script/Battle handler.cs
+++ Gerascophobia/Assets/Script/Battle handler.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Battlehandler : MonoBehaviour
 {
-    [SerializeField] private Transform pfCharacterBattle;
+    [SerializeField] private GameObject pfCharacterBattle;
 
     private void Start () {
+        if (pfCharacterBattle == null) {
+            Debug.LogError("pfCharacterBattle is not assigned in the inspector. Please assign it to the Battlehandler script.");
+            return;
+        }
         SpawnCharacter(true);
         SpawnCharacter(false);
     }
@@ -19,6 +23,5 @@ public class Battlehandler : MonoBehaviour
             position = new Vector3(+50,0);
         }
         Instantiate(pfCharacterBattle, position, Quaternion.identity);
-
     }
 }
--- Gerascophobia/Assets/Script/Character Battle.cs
+++ Gerascophobia/Assets/Script/Character Battle.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterBattle : MonoBehaviour {
-   private PlayerMovement playermovement;
+public class CharacterBattle : MonoBehaviour 
+{
 
-   private void Awake () {
-    playermovement = GetComponent<PlayerMovement>();
+    private PlayerMovement playermovement;
+
+   private void Awake () 
+   {
+        playermovement = GetComponent<PlayerMovement>();
    }
-    private void Start() {
-        PlayerMovement.movement.x(new Vector3(1,0));
+
+
+    private void Start() 
+    {
+        playermovement.transform.Translate(new Vector3(1, 0, 0));
     }
+
 } 
